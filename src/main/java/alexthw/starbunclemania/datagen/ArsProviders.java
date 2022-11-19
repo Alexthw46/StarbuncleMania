9035cbaa759a27ba5ff20235018bfb1ffb981860
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2022-11-19 12:06:06 +0100

    beta recipes

--- src/main/java/alexthw/starbunclemania/datagen/ArsProviders.java
+++ src/main/java/alexthw/starbunclemania/datagen/ArsProviders.java
@@ -11,17 +11,18 @@ import com.hollingsworth.arsnouveau.api.spell.AbstractEffect;
 import com.hollingsworth.arsnouveau.api.spell.AbstractSpellPart;
 import com.hollingsworth.arsnouveau.common.crafting.recipes.GlyphRecipe;
 import com.hollingsworth.arsnouveau.common.crafting.recipes.ImbuementRecipe;
-import com.hollingsworth.arsnouveau.common.datagen.ApparatusRecipeProvider;
-import com.hollingsworth.arsnouveau.common.datagen.GlyphRecipeProvider;
-import com.hollingsworth.arsnouveau.common.datagen.ImbuementRecipeProvider;
-import com.hollingsworth.arsnouveau.common.datagen.PatchouliProvider;
+import com.hollingsworth.arsnouveau.common.datagen.*;
 import com.hollingsworth.arsnouveau.common.datagen.patchouli.*;
+import com.hollingsworth.arsnouveau.setup.ItemsRegistry;
 import net.minecraft.data.CachedOutput;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DataProvider;
+import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.common.Tags;
@@ -52,6 +53,7 @@ public class ArsProviders {
             }
 
         }
+
         protected static Path getScribeGlyphPath(Path pathIn, Item glyph) {
             return pathIn.resolve("data/" + root + "/recipes/" + getRegistryName(glyph).getPath() + ".json");
         }
@@ -79,9 +81,25 @@ public class ArsProviders {
                     .build()
             );
 
+            recipes.add(builder()
+                    .withReagent(Ingredient.of(ItemTags.WOOL))
+                    .withPedestalItem(ItemsRegistry.AIR_ESSENCE)
+                    .withPedestalItem(2, ItemsRegistry.MAGE_FIBER)
+                    .withResult(ModRegistry.STARBALLON.get())
+                    .build()
+            );
+
+            recipes.add(builder()
+                    .withReagent(Items.SADDLE)
+                    .withPedestalItem(2, Ingredient.of(ItemTagProvider.SOURCE_GEM_TAG))
+                    .withPedestalItem(2, Ingredient.of(Tags.Items.NUGGETS_GOLD))
+                    .withResult(ModRegistry.STARSADDLE.get())
+                    .build()
+            );
+
             Path output = this.generator.getOutputFolder();
-            for (EnchantingApparatusRecipe g : recipes){
-                if (g != null){
+            for (EnchantingApparatusRecipe g : recipes) {
+                if (g != null) {
                     Path path = getRecipePath(output, g.getId().getPath());
                     DataProvider.saveStable(cache, g.asRecipe(), path);
                 }
@@ -89,8 +107,8 @@ public class ArsProviders {
 
         }
 
-        protected static Path getRecipePath(Path pathIn, String str){
-            return pathIn.resolve("data/"+ root +"/recipes/" + str + ".json");
+        protected static Path getRecipePath(Path pathIn, String str) {
+            return pathIn.resolve("data/" + root + "/recipes/" + str + ".json");
         }
 
         @Override
@@ -101,7 +119,7 @@ public class ArsProviders {
 
     public static class ImbuementProvider extends ImbuementRecipeProvider {
 
-        public ImbuementProvider(DataGenerator generatorIn){
+        public ImbuementProvider(DataGenerator generatorIn) {
             super(generatorIn);
         }
 
@@ -115,15 +133,15 @@ public class ArsProviders {
             */
 
             Path output = generator.getOutputFolder();
-            for(ImbuementRecipe g : recipes){
+            for (ImbuementRecipe g : recipes) {
                 Path path = getRecipePath(output, g.getId().getPath());
                 DataProvider.saveStable(cache, g.asRecipe(), path);
             }
 
         }
 
-        protected Path getRecipePath(Path pathIn, String str){
-            return pathIn.resolve("data/"+ root +"/recipes/" + str + ".json");
+        protected Path getRecipePath(Path pathIn, String str) {
+            return pathIn.resolve("data/" + root + "/recipes/" + str + ".json");
         }
 
         @Override
@@ -155,7 +173,7 @@ public class ArsProviders {
         }
 
         @Override
-        public void addBasicItem(ItemLike item, ResourceLocation category, IPatchouliPage recipePage){
+        public void addBasicItem(ItemLike item, ResourceLocation category, IPatchouliPage recipePage) {
             PatchouliBuilder builder = new PatchouliBuilder(category, item.asItem().getDescriptionId())
                     .withIcon(item.asItem())
                     .withPage(new TextPage(root + ".page." + getRegistryName(item.asItem()).getPath()))
@@ -216,10 +234,10 @@ public class ArsProviders {
 
         @Override
         public Path getPath(ResourceLocation category, String fileName) {
-            return this.generator.getOutputFolder().resolve("data/"+ root +"/patchouli_books/starbunclemania/en_us/entries/" + category.getPath() + "/" + fileName + ".json");
+            return this.generator.getOutputFolder().resolve("data/" + root + "/patchouli_books/starbunclemania/en_us/entries/" + category.getPath() + "/" + fileName + ".json");
         }
 
-        ImbuementPage ImbuementPage(ItemLike item){
+        ImbuementPage ImbuementPage(ItemLike item) {
             return new ImbuementPage(root + ":imbuement_" + getRegistryName(item.asItem()).getPath());
         }
 
--- src/main/java/alexthw/starbunclemania/datagen/ModRecipeProvider.java
+++ src/main/java/alexthw/starbunclemania/datagen/ModRecipeProvider.java
@@ -1,6 +1,9 @@
 package alexthw.starbunclemania.datagen;
 
 import static alexthw.starbunclemania.registry.ModRegistry.*;
+import static com.hollingsworth.arsnouveau.setup.ItemsRegistry.SOURCE_GEM;
+
+import alexthw.starbunclemania.registry.ModRegistry;
 import com.hollingsworth.arsnouveau.setup.BlockRegistry;
 import com.hollingsworth.arsnouveau.setup.ItemsRegistry;
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
@@ -9,8 +12,12 @@ import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.registries.RegistryObject;
 
 import java.util.function.Consumer;
 
@@ -21,13 +28,56 @@ public class ModRecipeProvider extends RecipeProvider {
 
     @Override
     protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
+        //items
+        shaped(STARBATTERY)
+                .define('R', Items.REDSTONE)
+                .define('C', Items.COPPER_INGOT)
+                .define('N', Items.GOLD_NUGGET)
+                .pattern(" N ")
+                .pattern("CRC")
+                .pattern("CRC")
+                .save(consumer);
 
-        ShapedRecipeBuilder.shaped(STARBATTERY.get()).define('R', Items.REDSTONE).define('C', Items.COPPER_INGOT).define('N', Items.GOLD_NUGGET).pattern(" N ").pattern("CRC").pattern("CRC").unlockedBy("has_journal", InventoryChangeTrigger.TriggerInstance.hasItems(ItemsRegistry.WORN_NOTEBOOK)).save(consumer);
-        ShapedRecipeBuilder.shaped(FLUID_JAR.get()).define('G', Items.GLASS).define('C', BlockRegistry.CASCADING_LOG.asItem()).define('W', ItemsRegistry.WATER_ESSENCE).pattern(" C ").pattern("GWG").pattern("CCC").unlockedBy("has_journal", InventoryChangeTrigger.TriggerInstance.hasItems(ItemsRegistry.WORN_NOTEBOOK)).save(consumer);
+        shaped(STARTRASH)
+                .define('I', Items.IRON_INGOT)
+                .define('N', Items.IRON_NUGGET)
+                .pattern(" N ")
+                .pattern("I I")
+                .pattern("NIN")
+                .save(consumer);
 
-
+        //blocks
+        shapedB(FLUID_JAR)
+                .define('G', Items.GLASS)
+                .define('C', BlockRegistry.CASCADING_LOG.asItem())
+                .define('W', ItemsRegistry.WATER_ESSENCE)
+                .pattern(" C ")
+                .pattern("GWG")
+                .pattern("CCC")
+                .save(consumer);
+        shapedB(FLUID_SOURCELINK)
+                .pattern(" S ")
+                .pattern("GBG")
+                .pattern(" S ")
+                .define('G', Tags.Items.INGOTS_GOLD)
+                .define('S', SOURCE_GEM)
+                .define('B', SOURCE_FLUID_BUCKET.get())
+                .save(consumer);
+        shapedB(SOURCE_CONDENSER)
+                .define('J', FLUID_JAR.get())
+                .define('S', SOURCE_GEM)
+                .pattern("SSS")
+                .pattern("SJS")
+                .pattern("SSS")
+                .save(consumer);
     }
 
+    public ShapedRecipeBuilder shaped(RegistryObject<Item> result){
+        return ShapedRecipeBuilder.shaped(result.get()).unlockedBy("has_journal", InventoryChangeTrigger.TriggerInstance.hasItems(ItemsRegistry.WORN_NOTEBOOK));
+    }
+    public ShapedRecipeBuilder shapedB(RegistryObject<Block> result){
+        return ShapedRecipeBuilder.shaped(result.get()).unlockedBy("has_journal", InventoryChangeTrigger.TriggerInstance.hasItems(ItemsRegistry.WORN_NOTEBOOK));
+    }
     public ShapelessRecipeBuilder shapelessBuilder(ItemLike result) {
         return shapelessBuilder(result, 1);
     }
